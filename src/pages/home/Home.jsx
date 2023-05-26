150e4a74e937d11b65049f93d9bbef1b1e48717b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-26 20:32:39 +0200

    Modified

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import { services } from '../../components/data/ServiceData'
+import { items } from '../../components/data/ItemsData'
+
+import { images } from '../../components/data/Marque'
+
+import Banner from '../../components/banner/Banner'
+import Offers from '../../components/offers/Offers'
+import Product from '../../components/product/Product'
+import Special from '../../components/special/Special'
+import PopularProducts from '../../components/popular-products/PopularProducts'
+import Marque from '../../components/marque/Marque'
+import Blog from '../../components/blog/Blog'
+import FamousProducts from '../../components/famous-products/FamousProducts'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <Banner />
+      <Offers services={services} items={items}/>
+      <Product />
+      <FamousProducts />
+      <Special />
+      <PopularProducts />
+      <Marque images={images} />
+      <Blog />
+    </div>
   )
 }
 
