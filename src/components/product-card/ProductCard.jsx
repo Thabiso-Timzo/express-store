0ab4606fa1db44c93a500c916757637a52b8b262
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-29 11:47:17 +0200

    Modified

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState }  from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Rating } from 'react-simple-star-rating'
 import { HiOutlineShoppingBag } from 'react-icons/hi'
 import { BiHeart } from 'react-icons/bi'
@@ -8,15 +8,18 @@ import { BsEye } from 'react-icons/bs'
 
 import blog from '../../assets/brands/samsung.png'
 
-const ProductCard = () => {
+const ProductCard = ({ grid }) => {
   const [ratingValue, setRatingValue] = useState(0)
+
+  let location = useLocation()
 
   const handleRating = (rate) => {
     setRatingValue(rate)
   }
   
   return (
-    <div className="col-3">
+    <>
+      <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
       <div className="product-card position-relative">
         <div className="product-image">
           <img src={blog} alt="" />
@@ -24,6 +27,28 @@ const ProductCard = () => {
             <h6 className="brand">Sumsang</h6>
             <h5 className="product-title">Samsung product</h5>
             <Rating onClick={handleRating} initialValue={ratingValue} size={window.innerWidth > 768 ? 20: 14} />
+            <p className={`product-desc ${grid === 12 ? "d-block" : "d-none"}`}>Lapto is running</p>
+            <p className="price">R400</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column">
+              <Link to='wishlist'><BiHeart/></Link>
+              <Link to=''><MdCompareArrows /></Link>
+              <Link to=''><BsEye /></Link>
+              <Link to=''><HiOutlineShoppingBag /></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
+      <div className="product-card position-relative">
+        <div className="product-image">
+          <img src={blog} alt="" />
+          <div className="product-details">
+            <h6 className="brand">Sumsang</h6>
+            <h5 className="product-title">Samsung product</h5>
+            <Rating onClick={handleRating} initialValue={ratingValue} size={window.innerWidth > 768 ? 20: 14} />
+            <p className={`product-desc ${grid === 12 ? "d-block" : "d-none"}`}>Lapto is running</p>
             <p className="price">R400</p>
           </div>
           <div className="action-bar position-absolute">
@@ -37,6 +62,7 @@ const ProductCard = () => {
         </div>
       </div>
     </div>
+    </>
   )
 }
 
--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Rating } from 'react-simple-star-rating'
 
+import BreadCrum from '../../components/bread-crump/BreadCrum'
+import Meta from '../../components/meta/Meta'
+import pic1 from '../../assets/product/2.jpeg'
+import StoreCol3 from '../../components/store-col-3/StoreCol3'
+import StoreCol9 from '../../components/store-col-9/StoreCol9'
+// 4:18:55
 const Store = () => {
+  const [ratingValue, setRatingValue] = useState(0)
+  const [grid, setGrid] = useState(3)
+
+  const handleRating = (rate) => {
+    setRatingValue(rate)
+  }
   return (
-    <div>Store</div>
+    <>
+      <Meta title={'Our store'} />
+      <BreadCrum title='Our Store'/>
+      <div className="store">
+        <div className="container-xxl">
+          <div className="row">
+            <StoreCol3 
+              pic1={pic1} 
+              ratingValue={ratingValue} 
+              handleRating={handleRating} 
+              Rating={Rating}
+            />
+            <StoreCol9 
+              grid={grid}
+              setGrid={setGrid}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
