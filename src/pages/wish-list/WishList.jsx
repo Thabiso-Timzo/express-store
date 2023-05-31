a0be0b8bca481c6aea0c347a46754a93fd1dd71e
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-31 12:10:02 +0200

    Modified UI

--- src/pages/wish-list/WishList.jsx
+++ src/pages/wish-list/WishList.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+import { RxCross2 } from 'react-icons/rx'
+
+import Meta from '../../components/meta/Meta'
+import BreadCrum from '../../components/bread-crump/BreadCrum'
+import product from '../../assets/product/4.jpeg'
 
 const WishList = () => {
   return (
-    <div>WishList</div>
+    <>
+      <Meta title={'Wish-list'} />
+      <BreadCrum title='Wish-list'/>
+      <div className="wishlist-wrapper">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="wishlist-card w-100 position-relative">
+                <RxCross2 size={200} className="position-absolute cross" />
+                <div className="wishlist-card-image">
+                  <img src={product} alt='' className="w-100" />
+                </div>
+                <div className="product-items">
+                  <h5 className="title">HP laptop, 4GB RAM</h5>
+                  <h6 className="price">R5500</h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="wishlist-card w-100 position-relative">
+                <RxCross2 size={200} className="position-absolute cross" />
+                <div className="wishlist-card-image">
+                  <img src={product} alt='' className="w-100" />
+                </div>
+                <div className="product-items">
+                  <h5 className="title">HP laptop, 4GB RAM</h5>
+                  <h6 className="price">R5500</h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="wishlist-card w-100 position-relative">
+                <RxCross2 size={200} className="position-absolute cross" />
+                <div className="wishlist-card-image">
+                  <img src={product} alt='' className="w-100" />
+                </div>
+                <div className="product-items">
+                  <h5 className="title">HP laptop, 4GB RAM</h5>
+                  <h6 className="price">R5500</h6>
+                </div>
+              </div>
+            </div><div className="col-3">
+              <div className="wishlist-card w-100 position-relative">
+                <RxCross2 size={200} className="position-absolute cross" />
+                <div className="wishlist-card-image">
+                  <img src={product} alt='' className="w-100" />
+                </div>
+                <div className="product-items">
+                  <h5 className="title">HP laptop, 4GB RAM</h5>
+                  <h6 className="price">R5500</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
