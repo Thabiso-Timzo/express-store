3e0530458c7345c4954e805101d116fbca360199
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-06-11 13:50:41 +0200

    Integrated products API

---
+++ src/features/products/productService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import { base_url, config } from "../../utils/axiosConfig";
+
+// Get all the products
+const getProducts = async (userData) => {
+    const response = await axios.get(`${base_url}/api/product/`)
+    if (response.data) {
+        return response.data
+    }
+}
+
+// Add product to wishlist
+const addToWishlist = async (prodId) => {
+    const response = await axios.put(`${base_url}/api/product/wishlist`, { prodId }, config)
+    if (response.data) {
+        return response.data
+    }
+}
+
+export const productService = {
+    getProducts,
+    addToWishlist
+}
---
+++ src/features/products/productSlice.js
@@ -0,0 +1,72 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+//import { toast } from 'react-toastify'
+
+import { productService } from './productService';
+
+// Get all products slice
+export const GetAllProducts = createAsyncThunk("product/get", async (thunkAPI) => {
+    try {
+        return await productService.getProducts();
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error);
+    }
+});
+
+// Add to wish list slice
+export const addToWishlist = createAsyncThunk("product/wishlist", async (prodId, thunkAPI) => {
+    try {
+        return await productService.addToWishlist(prodId);
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error);
+    }
+});
+
+const productState = {
+    product: "",
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ""
+}; 
+
+export const productSlice = createSlice({
+    name: "product",
+    initialState: productState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(GetAllProducts.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(GetAllProducts.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.product = action.payload;
+        })
+        .addCase(GetAllProducts.rejected, (state, action) => {
+            state.isError = true;
+            state.isLoading = false;
+            state.isSuccess = false;
+            state.message = action.error;
+        })
+        .addCase(addToWishlist.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(addToWishlist.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.addToWishlist = action.payload;
+            state.message = "Product added to wishlist."
+        })
+        .addCase(addToWishlist.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.isSuccess = false;
+            state.message = action.error;
+        })
+    }
+});
+
+export default productSlice.reducer;
