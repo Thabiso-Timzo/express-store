623897696a311042c2451797acbd1a0dd15dc369
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-06-11 12:15:29 +0200

    Modified

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,1 +1,14 @@
 export const base_url = "http://localhost:8080"
+
+const getTokenFromLocalStorage = localStorage.getItem("customer") 
+    ? JSON.parse(localStorage.getItem("customer")) 
+    : null
+
+export const config = {
+    headers: {
+        Authorization: `Bearer ${
+            getTokenFromLocalStorage !== null ? getTokenFromLocalStorage.token : null
+        }`,
+        Accept: "application/json"
+    }
+}
